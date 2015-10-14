c1f6d3a4ff686b2c70aa6e575c02cc2092c793f1
tell <tell@5638313d-7e05-0410-91fb-e803f45e2111>
2015-10-14 03:46:53 +0000

    fix up WFUV, add FUV2 add more stations
    git-svn-id: svn+ssh://ariel/home/svn/astreambox@41 5638313d-7e05-0410-91fb-e803f45e2111

--- src/SgtStationList.py
+++ src/SgtStationList.py
@@ -43,18 +43,23 @@ class SgtStationList:
 
     def initfixed(self):  
         "init from fixed list for early testing"
-        self.list.append( SgtStationEntry("WFUV", "http://wfuv-music.streamguys.org:80/music-hi"))
+        self.list.append( SgtStationEntry("WFUV", "http://wfuv-music.streamguys.org:80/onair-hi"))
+        self.list.append( SgtStationEntry("FUV2", "http://wfuv-music.streamguys.org:80/music-hi"))
         self.list.append( SgtStationEntry("WKNC", "http://wknc.sma.ncsu.edu:8000/wknchq.ogg"));
         self.list.append( SgtStationEntry("KCRW", "http://kcrw.ic.llnwd.net/stream/kcrw_live"));
         self.list.append( SgtStationEntry("KSDS", "http://listen.jazz88.org/ksds.mp3"));
         self.list.append( SgtStationEntry("WBGO", "http://wbgo.streamguys.net:8000"));
         self.list.append( SgtStationEntry("WWOZ", "http://50.31.135.43:80/wwoz-hi.mp3"));
-        self.list.append( SgtStationEntry("WAMU", "http://wamu-1.streamguys.com:80"));
         self.list.append( SgtStationEntry("WNYC", "http://fm939.wnyc.org/wnycfm.aac"));
 
+        self.list.append( SgtStationEntry("WAMU", "http://wamu-1.streamguys.com:80"));
         self.list.append( SgtStationEntry("KPCC", "http://live.scpr.org/kpcclive"));
         self.list.append( SgtStationEntry("WSHA", "http://live.wshafm.org/WSHA?MSWMExt=.asf"));
         self.list.append( SgtStationEntry("WNCU", "http://stream.publicbroadcasting.net/production/mp3/wncu/local-wncu-974743.mp3"));
+        self.list.append( SgtStationEntry("KLCK", "http://edgev1.den.echo.liquidcompass.net/KLCKFMMP3?ats=1"));
+        self.list.append( SgtStationEntry("WJMU", "mms://streaming.millikin.edu/wjmu"));
+        self.list.append( SgtStationEntry("WEXT", "http://live.str3am.com:2080/wext1"));
+        self.list.append( SgtStationEntry("WGFR", "http://wgfr.streamon.fm:8000/WGFR-24k.aac"));
 
 
     def writefile(self):
